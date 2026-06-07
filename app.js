aecd03defe7aef157c68be35e1b9c5d722cf9a72
Yong lin <YL4648@Columbia.edu>
2026-06-07 09:45:40 -0700

    Fix station-local time labels

--- app.js
+++ app.js
@@ -302,7 +302,7 @@ function shortTraceTime(value) {
 }
 
 function parseIsoLocalParts(value) {
-  const match = String(value).match(/^\\d{4}-(\\d{2})-(\\d{2})T(\\d{2}):(\\d{2})/);
+  const match = String(value).match(/^\d{4}-(\d{2})-(\d{2})T(\d{2}):(\d{2})/);
   if (!match) return null;
   const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
   return {
